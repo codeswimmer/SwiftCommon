0b1cdd9757e3f41ed38e860971d39b287f57172d
Keith Ermel <keith.ermel@me.com>
2014-10-01 16:44:53 -0700

    Abandoning work on parallax table for now

--- SwiftCommonExample/SwiftCommonExample/Source/Examples/ParallaxHeaderTableViewController.swift
+++ SwiftCommonExample/SwiftCommonExample/Source/Examples/ParallaxHeaderTableViewController.swift
@@ -13,7 +13,8 @@ import SwiftCommon
 class ParallaxHeaderTableViewController: UITableViewController {
 
     var items: [String]?
-    var parallaxView: UIView?
+    var headerImage: UIImageView?
+    var headerImageOffsetY: CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +23,23 @@ class ParallaxHeaderTableViewController: UITableViewController {
         tableView.rowHeight = 50.0
         tableView.hideEmptyCells()
         
-//        tableView.tableHeaderView = ViewBuilder
-//            .box((tableView.bounds.width, 50.0))
-//            .color(UIColor.blackColor()).view
+        tableView.tableHeaderView = ViewBuilder
+            .box((tableView.bounds.width, 50.0))
+            .color(UIColor.blackColor()).view
 
-        parallaxView = ViewBuilder.box((320.0, 350.0)).color(UIColor.redColor()).view
-        println("parallaxView: \(parallaxView!.frame)")
-        tableView.insertSubview(parallaxView!, atIndex: 0)
+//        headerImage = UIImageView(image: UIImage(named: "inout"))
+        let r = _G.rectWithSize(_G.size(view.bounds.width, 50.0))
+        headerImage = UIImageView(frame: r)
+        headerImage!.image = UIImage(named: "inout")
+        headerImage!.frame = r
+        headerImage!.contentMode = UIViewContentMode.ScaleAspectFill
+        
+//        println("headerImage.frame: \(headerImage!.frame)")
+//        headerImageOffsetY = -(headerImage!.bounds.size.height) + 50.0
+//        headerImage!.frame = _G.rectFrom(headerImage!.frame, withY: headerImageOffsetY)
+//        println("headerImage.frame: \(headerImage!.frame)")
+        
+        view.insertSubview(headerImage!, belowSubview: tableView)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -48,10 +59,27 @@ class ParallaxHeaderTableViewController: UITableViewController {
     
     // MARK: UIScrollViewDelegate
     override func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        println(">>> \(tableView.contentOffset) \(tableView.contentSize)")
+        
         if let headerView = tableView.tableHeaderView {
-            let b = headerView.bounds
-            let origin = CGPointMake(b.origin.x, max(0, min(CGRectGetHeight(b), tableView.contentOffset.y)))
+            if let imageView = headerImage {
+                let offsetY = tableView.contentOffset.y
+                var r = imageView.frame
+                
+                if offsetY < 0.0 {
+                    r.origin.y = headerImageOffsetY - (offsetY / 8)
+                }
+                else {
+                    r.origin.y = headerImageOffsetY - offsetY
+                }
+                
+                imageView.frame = r
+            }
             
+            
+//            let b = headerView.bounds
+//            let origin = CGPointMake(b.origin.x, max(0, min(CGRectGetHeight(b), tableView.contentOffset.y)))
 //            let y = min(max(abs(tableView.contentOffset.y), 50.0), 250.0)
 //            headerView.frame = _G.rectFrom(headerView.frame, withHeight: abs(y) + 50.0)
 //            println(">>> y: \(y)); frame: \(headerView.frame);  \(tableView.contentOffset); \(tableView.contentSize)")
